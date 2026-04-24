5706b10ebf09506eac2d626d98f08034dc2349ee
SW van Heerden <swvheerden@gmail.com>
2026-04-24 15:07:38 +0200

    upgrade to newer versions

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(feature = "nightly", feature(external_doc))]
-#![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
+#![doc = include_str!("../README.md")]
 #![doc(html_logo_url = "https://doc.dalek.rs/assets/dalek-logo-clear.png")]
-// put this after the #![doc(..)] so it appears as a footer:
-//! Note that docs will only build on nightly Rust until
-//! [RFC 1990 stabilizes](https://github.com/rust-lang/rust/issues/44732).
 
 #[cfg(target_endian = "big")]
 compile_error!(
